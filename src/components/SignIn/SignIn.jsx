fa2b316eea68731fe2df44d789a270f7fa69fe48
MrDean <Paul---@users.noreply.github.com>
2020-02-10 18:32:56 -0700

    add clarification to deployed app. Backend not connected

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,55 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const SignInForm = ({ handleRouteChange }) => {
   return (
-    <article className='br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5'>
-      <main className='pa4 black-80'>
-        <div className='measure '>
-          <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-            <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
-            <div className='mt3'>
-              <label className='db fw6 lh-copy f6' htmlFor='email-address'>
-                Email
-              </label>
+    <>
+      <article
+        style={{ maxWidth: "1000px", width: "90%" }}
+        className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5"
+      >
+        <h1>
+          The backend of this game has not been connected as of yet, so feel
+          free to simply click the 'Sign In' button to play the game.
+        </h1>
+        <h2 className=""> Enjoy!</h2>
+      </article>
+      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
+        <main className="pa4 black-80">
+          <div className="measure ">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                />
+              </div>
+            </fieldset>
+            <div className="">
               <input
-                className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
-                type='email'
-                name='email-address'
-                id='email-address'
+                onClick={() => handleRouteChange("home")}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
               />
             </div>
-            <div className='mv3'>
-              <label className='db fw6 lh-copy f6' htmlFor='password'>
-                Password
-              </label>
-              <input
-                className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
-                type='password'
-                name='password'
-                id='password'
+            <div className="lh-copy mt3">
+               <input
+                onClick={() => handleRouteChange("register")}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
               />
             </div>
-          </fieldset>
-          <div className=''>
-            <input
-              onClick={() => handleRouteChange('home')}
-              className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib'
-              type='submit'
-              value='Sign in'
-            />
           </div>
-          <div className='lh-copy mt3'>
-            <a
-              href='#0'
-              className='b f6 link dim black db'
-              onClick={() => handleRouteChange('register')}
-            >
-              Register
-            </a>
-          </div>
-        </div>
-      </main>
-    </article>
+        </main>
+      </article>
+    </>
   );
 };
 
