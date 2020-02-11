47b15b03bc372e255d68d4ff19575e20de3228e1
MrDean <Paul---@users.noreply.github.com>
2020-02-10 18:11:14 -0700

    Updates

---
+++ precache-manifest.25a0290e34a06996a062f4d70bea8260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3793358c7b7e4a0c51d0a717d1696f74",
+    "url": "/facial-recognition/index.html"
+  },
+  {
+    "revision": "e5d800d14fd6bcfbb552",
+    "url": "/facial-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "21fbde6b73c388e9166b",
+    "url": "/facial-recognition/static/css/main.c207997f.chunk.css"
+  },
+  {
+    "revision": "e5d800d14fd6bcfbb552",
+    "url": "/facial-recognition/static/js/2.87a32d82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facial-recognition/static/js/2.87a32d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21fbde6b73c388e9166b",
+    "url": "/facial-recognition/static/js/main.8c887e2a.chunk.js"
+  },
+  {
+    "revision": "4967471a9af3eb024a48",
+    "url": "/facial-recognition/static/js/runtime-main.ec0edf3d.js"
+  }
+]);
