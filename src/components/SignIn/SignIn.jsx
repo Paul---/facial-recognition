31b03f264c63316c2d02600308c1faf67defaa7e
MrDean <Paul---@users.noreply.github.com>
2020-04-22 16:54:40 -0600

    improve login error handling

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,47 +1,48 @@
-import React, { useState } from "react";
-import BannerMessage from "../BannerMessage/BannerMessage";
+import React, { useState } from 'react';
+import BannerMessage from '../BannerMessage/BannerMessage';
 
 const SignInForm = ({ handleRouteChange, loadUserData }) => {
-  const [email, changeEmail] = useState("");
-  const [password, changePassword] = useState("");
+  const [email, changeEmail] = useState('');
+  const [password, changePassword] = useState('');
   const baseUrl = `https://young-brushlands-14538.herokuapp.com/`;
   const [loginError, changeLoginError] = useState(false);
   const [wrongCredentials, changeWrongCredentials] = useState(false);
 
-  const onEmailChange = e => {
+  const onEmailChange = (e) => {
     changeEmail(e.target.value);
   };
 
-  const onPasswordChange = e => {
+  const onPasswordChange = (e) => {
     changePassword(e.target.value);
   };
 
   const onSubmit = () => {
-    if (!email || !password || !email.includes("@")) {
+    if (!email || !password || !email.includes('@')) {
       changeLoginError(true);
       changeWrongCredentials(false);
       return;
     }
+    changeLoginError(false);
     fetch(`${baseUrl}login`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ password, email })
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ password, email }),
     })
-      .then(res => res.json())
-      .then(userData => {
+      .then((res) => res.json())
+      .then((userData) => {
         if (userData !== `Error Logging In`) {
           loadUserData(userData);
-          handleRouteChange("home");
+          handleRouteChange('home');
           changeLoginError(false);
         }
       })
-      .catch(e => {
+      .catch((e) => {
         changeLoginError(true);
       });
     setTimeout(() => {
       changeLoginError(false);
       changeWrongCredentials(true);
-    }, 2000);
+    }, 3000);
   };
 
   return (
@@ -55,55 +56,55 @@ const SignInForm = ({ handleRouteChange, loadUserData }) => {
       {wrongCredentials ? (
         <BannerMessage message={`Please Check Your Login Credentials`} />
       ) : null}
-      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
-        <main className="pa4 black-80">
-          <div className="measure ">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+      <article className='br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5'>
+        <main className='pa4 black-80'>
+          <div className='measure '>
+            <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+              <legend className='f1 fw6 ph0 mh0'>Sign In</legend>
+              <div className='mt3'>
+                <label className='db fw6 lh-copy f6' htmlFor='email-address'>
                   Email
                 </label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
                   onChange={onEmailChange}
-                  type="email"
-                  name="email-address"
-                  id="email-address"
+                  type='email'
+                  name='email-address'
+                  id='email-address'
                 />
               </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
+              <div className='mv3'>
+                <label className='db fw6 lh-copy f6' htmlFor='password'>
                   Password
                 </label>
                 <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
                   onChange={onPasswordChange}
-                  onKeyPress={e => {
-                    if (e.key === "Enter") {
+                  onKeyPress={(e) => {
+                    if (e.key === 'Enter') {
                       onSubmit(e);
                     }
                   }}
-                  type="password"
-                  name="password"
-                  id="password"
+                  type='password'
+                  name='password'
+                  id='password'
                 />
               </div>
             </fieldset>
-            <div className="lh-copy mt3">
+            <div className='lh-copy mt3'>
               <input
                 onClick={onSubmit}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w4"
-                type="submit"
-                value="Sign in"
+                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w4'
+                type='submit'
+                value='Sign in'
               />
             </div>
-            <div className="lh-copy mt3">
+            <div className='lh-copy mt3'>
               <input
-                onClick={() => handleRouteChange("register")}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w4"
-                type="submit"
-                value="Register"
+                onClick={() => handleRouteChange('register')}
+                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w4'
+                type='submit'
+                value='Register'
               />
             </div>
           </div>
